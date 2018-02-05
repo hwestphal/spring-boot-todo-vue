9c00f28cd4b3d1f9d5e3e40a8af37d1140e8e097
Harald Westphal <h.westphal@gmx.de>
2018-02-05 18:13:17 +0100

    prepare for template compiling

--- frontend/src/main/typescript/main.ts
+++ frontend/src/main/typescript/main.ts
@@ -5,13 +5,14 @@ import "../css/styles.css";
 import "./autocomplete";
 import { default as todolist, ITodo } from "./todolist";
 
-export function main(todos: ITodo[]) {
+export function main(todos: ITodo[], action: string, element: string) {
   todolist(todos,
     ["Wash the car",
       "Learn Typescript",
       "Get a life",
       "Feed the dog",
       "Feed the cat"],
-    "#todolist-template",
-    "#todolist");
+      action,
+      element,
+    "#todolist-template");
 }
--- frontend/src/main/typescript/todolist.ts
+++ frontend/src/main/typescript/todolist.ts
@@ -13,79 +13,82 @@ function normalize(s: string) {
   return s.trim().toLowerCase().split(/\s+/).join(" ");
 }
 
-export default (todos: ITodo[], suggestions: string[] = [], template?: string, el?: string) => new Vue({
-  el,
-  template,
+export default (
+  todos: ITodo[], suggestions: string[] = [],
+  action: string = "", el?: string, template?: string) => new Vue({
+    el,
+    template,
 
-  data: {
-    changed: false,
-    newTodo: "",
-    todos: clone(todos),
-  },
-
-  filters: {
-    json: JSON.stringify,
-  },
-
-  computed: {
-    openTodos(): ITodo[] {
-      return this.todos.filter((t) => !t.completed);
+    data: {
+      action,
+      changed: false,
+      newTodo: "",
+      todos: clone(todos),
     },
 
-    doneTodos(): ITodo[] {
-      return this.todos.filter((t) => t.completed);
+    filters: {
+      json: JSON.stringify,
     },
 
-    valid(): boolean {
-      return this.newTodo.length > 3;
+    computed: {
+      openTodos(): ITodo[] {
+        return this.todos.filter((t) => !t.completed);
+      },
+
+      doneTodos(): ITodo[] {
+        return this.todos.filter((t) => t.completed);
+      },
+
+      valid(): boolean {
+        return this.newTodo.length > 3;
+      },
+
+      openSuggestions(): string[] {
+        return suggestions.filter((s) => !this.todos.some((t) => normalize(t.title) === normalize(s)));
+      },
     },
 
-    openSuggestions(): string[] {
-      return suggestions.filter((s) => !this.todos.some((t) => normalize(t.title) === normalize(s)));
-    },
-  },
-
-  methods: {
-    close(todo: ITodo) {
-      todo.completed = true;
-      this.changed = true;
-    },
-
-    open(todo: ITodo) {
-      todo.completed = false;
-      this.changed = true;
-    },
-
-    remove(todo: ITodo) {
-      this.todos.splice(this.todos.indexOf(todo), 1);
-      this.changed = true;
-    },
-
-    closeAll() {
-      this.todos.forEach((todo) => {
+    methods: {
+      close(todo: ITodo) {
         todo.completed = true;
-      });
-      this.changed = true;
-    },
-
-    addNewTodo() {
-      if (this.valid) {
-        this.todos.push({
-          completed: false,
-          title: this.newTodo,
-        });
-        this.newTodo = "";
         this.changed = true;
-      }
-    },
+      },
 
-    reset() {
-      this.todos = clone(todos);
-      this.changed = false;
-    },
+      open(todo: ITodo) {
+        todo.completed = false;
+        this.changed = true;
+      },
 
-    save() {
-      (this.$refs.form as HTMLFormElement).submit();
+      remove(todo: ITodo) {
+        this.todos.splice(this.todos.indexOf(todo), 1);
+        this.changed = true;
+      },
+
+      closeAll() {
+        this.todos.forEach((todo) => {
+          todo.completed = true;
+        });
+        this.changed = true;
+      },
+
+      addNewTodo() {
+        if (this.valid) {
+          this.todos.push({
+            completed: false,
+            title: this.newTodo,
+          });
+          this.newTodo = "";
+          this.changed = true;
+        }
+      },
+
+      reset() {
+        this.todos = clone(todos);
+        this.changed = false;
+      },
+
+      save() {
+        (this.$refs.form as HTMLFormElement).submit();
+      },
     },
-  },
-});
+  });
