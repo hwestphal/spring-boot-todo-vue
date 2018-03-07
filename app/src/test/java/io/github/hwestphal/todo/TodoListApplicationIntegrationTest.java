b4bd41a6a83b0f70f90644b0b768313a8a491f3d
Harald Westphal <harald.westphal@kuehne-nagel.com>
2018-03-07 14:10:35 +0100

    increase java code coverage

--- app/src/test/java/io/github/hwestphal/todo/TodoListApplicationIntegrationTest.java
+++ app/src/test/java/io/github/hwestphal/todo/TodoListApplicationIntegrationTest.java
@@ -6,7 +6,10 @@ import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Consumer;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +17,13 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, properties = "logging.level.com.querydsl.sql=DEBUG")
@@ -24,6 +32,9 @@ public class TodoListApplicationIntegrationTest {
 
     @Autowired
     private TestRestTemplate restTemplate;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Test
     public void shouldAddAndChangeAndRemoveTodo() {
@@ -48,7 +59,28 @@ public class TodoListApplicationIntegrationTest {
     }
 
     @Test
-    public void shouldOverwriteAndRemoveAllTodos() {
+    public void shouldOverwriteAndRemoveAllTodosWithPut() {
+        shouldOverwriteAndRemoveAllTodos(ts -> restTemplate.put("/", ts));
+    }
+
+    @Test
+    public void shouldOverwriteAndRemoveAllTodosWithPost() {
+        shouldOverwriteAndRemoveAllTodos(ts -> restTemplate.postForEntity("/", asJsonFormField("todos", ts), Void.class));
+    }
+
+    private HttpEntity<?> asJsonFormField(String fieldName, Object obj) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
+        try {
+            map.add(fieldName, objectMapper.writeValueAsString(obj));
+        } catch (JsonProcessingException e) {
+            throw new IllegalArgumentException(e);
+        }
+        return new HttpEntity<MultiValueMap<String, String>>(map, headers);
+    }
+
+    private void shouldOverwriteAndRemoveAllTodos(Consumer<List<Todo>> todoUploader) {
         Todo todo1 = Todo.builder().title("todo1").build();
         todo1 = restTemplate.getForObject(restTemplate.postForLocation("/", todo1), Todo.class);
         Todo todo2 = Todo.builder().title("todo2").build();
@@ -58,7 +90,7 @@ public class TodoListApplicationIntegrationTest {
 
         Todo todo1a = Todo.builder().id(todo1.getId()).version(todo1.getVersion()).title("todo1a").build();
         Todo todo3 = Todo.builder().title("todo3").completed(true).build();
-        restTemplate.put("/", Arrays.asList(todo3, todo1a));
+        todoUploader.accept(Arrays.asList(todo3, todo1a));
         todos = restTemplate.getForObject("/", List.class);
         assertThat(todos).hasSize(2);
         @SuppressWarnings("unchecked")
