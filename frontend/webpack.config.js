ed7b523ebc83574ae6dd70ef8240a60b187c8398
Harald Westphal <h.westphal@gmx.de>
2018-04-30 20:32:40 +0200

    migrate to webpack 4 and vue-loader 15

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,73 +1,93 @@
-const fs = require('fs');
 const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const ShakePlugin = require('webpack-common-shake').Plugin;
-const Viz = require('viz.js');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { VueLoaderPlugin } = require('vue-loader');
 
-module.exports = {
-    entry: {
-        main: './src/main/typescript/main.ts'
-    },
-    module: {
-        rules: [{
-            test: /\.ts$/,
-            use: {
-                loader: 'ts-loader',
-                options: {
-                    appendTsSuffixTo: [/\.vue$/]
-                }
-            }
-        }, {
-            test: /\.vue$/,
-            use: {
-                loader: 'vue-loader',
-                options: {
-                    loaders: {
-                        css: ExtractTextPlugin.extract({
-                            use: 'css-loader?sourceMap',
-                            fallback: 'vue-style-loader'
-                        }),
-                        scss: ExtractTextPlugin.extract({
-                            use: ['css-loader?sourceMap', 'sass-loader?sourceMap'],
-                            fallback: 'vue-style-loader'
-                        })
+module.exports = function (env, args = {}) {
+    const prod = args.mode === 'production';
+    return {
+        mode: 'development',
+        devtool: prod ? 'source-map' : 'eval-source-map',
+        entry: {
+            main: './src/main/typescript/main.ts'
+        },
+        module: {
+            rules: [{
+                test: /\.ts$/,
+                use: {
+                    loader: 'ts-loader',
+                    options: {
+                        appendTsSuffixTo: [/\.vue$/]
                     }
                 }
-            }
-        }, {
-            test: /\.s?css$/,
-            use: ExtractTextPlugin.extract({
-                use: ['css-loader?sourceMap', 'sass-loader?sourceMap'],
-                fallback: 'style-loader'
-            })
-        }, {
-            test: /\.(woff|woff2|eot|ttf)$/,
-            use: {
-                loader: 'file-loader',
-                options: {
-                    outputPath: 'fonts/',
-                    name: '[name].[ext]',
-                    publicPath: '../fonts'
+            }, {
+                test: /\.vue$/,
+                use: 'vue-loader'
+            }, {
+                test: /\.s?css$/,
+                oneOf: [
+                    {
+                        resourceQuery: /module/,
+                        use: [
+                            prod ? MiniCssExtractPlugin.loader : 'vue-style-loader',
+                            {
+                                loader: 'css-loader',
+                                options: {
+                                    modules: true,
+                                    localIdentName: '[local]_[hash:base64:8]',
+                                    sourceMap: true
+                                }
+                            },
+                            {
+                                loader: 'sass-loader',
+                                options: {
+                                    sourceMap: true
+                                }
+                            }
+                        ]
+                    },
+                    {
+                        use: [
+                            prod ? MiniCssExtractPlugin.loader : 'vue-style-loader',
+                            {
+                                loader: 'css-loader',
+                                options: {
+                                    sourceMap: true
+                                }
+                            },
+                            {
+                                loader: 'sass-loader',
+                                options: {
+                                    sourceMap: true
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }, {
+                test: /\.(woff|woff2|eot|ttf)$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        outputPath: 'fonts/',
+                        name: '[name].[ext]',
+                        publicPath: '../fonts'
+                    }
                 }
-            }
-        }]
-    },
-    plugins: [
-        new ExtractTextPlugin('css/[name].css'),
-        new ShakePlugin({
-            onGraph: (graph) => fs.writeFile(path.resolve(__dirname, 'target/webpack-modules.svg'),
-                Viz(graph.replace(/\\/g, '/')),
-                'utf8',
-                (err) => { if (err) console.error(err); })
-        }),
-    ],
-    resolve: {
-        extensions: ['.js', '.ts'],
-    },
-    output: {
-        filename: 'js/[name].js',
-        path: path.resolve(__dirname, 'target/classes/static'),
-        library: '__[name]'
-    },
-    devtool: 'source-map'
-};
+            }]
+        },
+        plugins: [
+            new VueLoaderPlugin(),
+            new MiniCssExtractPlugin({
+                filename: 'css/[name].css'
+            })
+        ],
+        resolve: {
+            extensions: ['.js', '.ts'],
+        },
+        output: {
+            filename: 'js/[name].js',
+            path: path.resolve(__dirname, 'target/classes/static'),
+            library: '__[name]'
+        }
+    };
+}
