fc440ad92ebb8d39c87cd5c05aa5622393723ce7
Harald Westphal <harald.westphal@kuehne-nagel.com>
2018-05-18 15:43:30 +0200

    remove unneeded stubbing of $style attribute

--- frontend/src/main/typescript/main.test.ts
+++ frontend/src/main/typescript/main.test.ts
@@ -1,15 +1,6 @@
 import Vue from "vue";
 import main from "./main";
 
-// add $style attribute for uncompiled CSS modules
-Vue.mixin({
-    computed: {
-        $style() {
-            return {};
-        },
-    },
-});
-
 test("main function runs without warnings or errors", () => {
     const warn = jest.spyOn(global.console, "warn");
     const error = jest.spyOn(global.console, "error");
