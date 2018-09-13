fd8b231f7dcd8bbe7bfd55283bac92ea74674176
Harald Westphal <h.westphal@gmx.de>
2018-09-13 18:45:03 +0200

    Add a reactive decorator

---
+++ frontend/src/main/typescript/reactive.spec.ts
@@ -0,0 +1,212 @@
+import Vue from "vue";
+import { reactive } from "./reactive";
+
+describe("reactive decorator", () => {
+    it("initializes class correctly", () => {
+        @reactive
+        class TC {
+            a = 1;
+            constructor(public b: number) { }
+        }
+        expect(TC.name).toBe("TC");
+        const c = new TC(2);
+        expect(c.a).toBe(1);
+        expect(c.b).toBe(2);
+    });
+
+    it("preserves full interface", () => {
+        const m5 = jest.fn();
+        const m5super = jest.fn();
+        class B {
+            e = 5;
+            private d = 4;
+
+            constructor(public a: number) { }
+
+            get dp() {
+                return this.d;
+            }
+
+            set dp(v: number) {
+                this.d = v;
+            }
+
+            m2() {
+                this.d += 1;
+            }
+
+            protected m3() {
+                this.e += 1;
+            }
+
+            protected m5() {
+                m5super();
+            }
+        }
+        @reactive
+        class C extends B {
+            b = 2;
+            private c = 3;
+
+            get cp() {
+                return this.c;
+            }
+
+            set cp(v: number) {
+                this.c = v;
+            }
+
+            m1() {
+                this.c += 1;
+            }
+
+            m4() {
+                this.m3();
+            }
+
+            m5() {
+                super.m5();
+                m5();
+            }
+        }
+        const c = new C(1);
+        expect(c.a).toBe(1);
+        expect(c.b).toBe(2);
+        expect(c.cp).toBe(3);
+        expect(c.dp).toBe(4);
+        expect(c.e).toBe(5);
+        c.cp = 10;
+        c.m1();
+        expect(c.cp).toBe(11);
+        c.dp = 20;
+        c.m2();
+        expect(c.dp).toBe(21);
+        c.m4();
+        expect(c.e).toBe(6);
+        c.m5();
+        expect(m5).toBeCalled();
+        expect(m5super).toBeCalled();
+    });
+
+    it("makes class reactive", async () => {
+        @reactive
+        class C {
+            a = { v: 1 };
+            constructor(public b: any) { }
+        }
+        const obj = { v: 2 };
+        const c = new C(obj);
+        const watchA = jest.fn();
+        const watchB = jest.fn();
+        const vm = new Vue({
+            inject: {
+                c: {
+                    default: c,
+                },
+            },
+            watch: {
+                "c.a.v": watchA,
+                "c.b.v": watchB,
+            },
+        });
+        c.a.v = 3;
+        c.b.v = 4;
+        await vm.$nextTick();
+        expect(watchA).toBeCalledWith(3, 1);
+        expect(watchB).not.toBeCalled();
+    });
+
+    it("supports reactive delegation", async () => {
+        @reactive
+        class B {
+            a = 1;
+        }
+        @reactive
+        class C {
+            constructor(private b: B) { }
+            get a() {
+                return this.b.a;
+            }
+        }
+        const i = new B();
+        const c = new C(i);
+        const watcher = jest.fn();
+        const vm = new Vue({
+            inject: {
+                c: {
+                    default: c,
+                },
+            },
+            watch: {
+                "c.a": watcher,
+            },
+        });
+        i.a = 2;
+        await vm.$nextTick();
+        expect(watcher).toBeCalledWith(2, 1);
+    });
+
+    it("rejects reactive super class", () => {
+        @reactive
+        class Base { }
+        expect(() => {
+            @reactive
+            class C extends Base { }
+        }).toThrow(TypeError);
+    });
+
+    it("uses extended Vue constructor", () => {
+        const m = jest.fn();
+        const vueConstructor = Vue.extend({
+            mixins: [
+                {
+                    methods: {
+                        mixinMethod: m,
+                    },
+                },
+            ],
+        });
+        @reactive({ vueConstructor })
+        class C { }
+        const c = new C();
+        (c as any).mixinMethod();
+        expect(m).toBeCalled();
+    });
+
+    it("registers after creation callback", () => {
+        @reactive({ afterCreation: "init" })
+        class C {
+            a = 1;
+            init() { this.a += 1; }
+        }
+        const c = new C();
+        expect(c.a).toBe(2);
+        expect(() => c.init()).toThrowError();
+        expect(c.a).toBe(2);
+
+        @reactive({ afterCreation: "init" })
+        class D {
+            constructor() { this.init(); }
+            private init() { }
+        }
+        expect(() => new D()).toThrowError();
+
+        abstract class E {
+            abstract a: number;
+            protected init() { this.a += 1; }
+        }
+        @reactive({ afterCreation: "init" })
+        class F extends E {
+            a = 1;
+            init() { super.init(); }
+        }
+        const f = new F();
+        expect(f.a).toBe(2);
+
+        expect(() => {
+            @reactive({ afterCreation: "init" })
+            class G {
+            }
+        }).toThrow(TypeError);
+    });
+});
---
+++ frontend/src/main/typescript/reactive.ts
@@ -0,0 +1,105 @@
+import Vue, { VueConstructor } from "vue";
+
+interface IConstructable<T> {
+    new(...args: any[]): T;
+}
+
+interface IDecoratorOptions {
+    afterCreation?: string;
+    vueConstructor?: VueConstructor;
+}
+
+class ReactiveBase { }
+
+function decorate<T>(
+    ctor: IConstructable<T>,
+    { afterCreation, vueConstructor = Vue }: IDecoratorOptions = {}): IConstructable<T> {
+
+    // determine methods and computed properties (i.e. getter/setter)
+    const methods: any = {};
+    const computed: any = {};
+    let created!: () => void;
+
+    let proto = ctor.prototype;
+    while (proto && proto !== Object.prototype) {
+        if (proto instanceof ReactiveBase) {
+            throw new TypeError("A reactive class must not have a reactive super class");
+        }
+        for (const key of Object.getOwnPropertyNames(proto)) {
+            if (key !== "constructor" && !(key in methods || key in computed)) {
+                const pd = Object.getOwnPropertyDescriptor(proto, key)!;
+                if (pd.value) {
+                    if (key === afterCreation) {
+                        created = pd.value;
+                        methods[key] = proto[key] = () => {
+                            throw new Error("After creation hook must not be called directly");
+                        };
+                    } else {
+                        methods[key] = pd.value;
+                    }
+                } else {
+                    computed[key] = {
+                        get: pd.get,
+                        set: pd.set,
+                    };
+                }
+            }
+        }
+        proto = Object.getPrototypeOf(proto);
+    }
+    if (afterCreation && !created) {
+        throw new TypeError(`"${afterCreation}" method not found`);
+    }
+
+    class C extends ReactiveBase {
+        constructor(...args: any[]) {
+            super();
+
+            // determine names of reactive (i.e. data) properties
+            // all properties which are initialized when calling the default constructor are considered reactive
+            // all others are considered to be not reactive (i.e. being constructor injected)
+            const initObj = new ctor();
+            const reactiveProps: string[] = [];
+            for (const key in initObj) {
+                if (initObj[key] !== undefined) {
+                    reactiveProps.push(key);
+                }
+            }
+
+            const data: any = {};
+            const inject: any = {};
+            const obj = new ctor(...args);
+            for (const key in obj) {
+                if (reactiveProps.indexOf(key) > -1) {
+                    data[key] = obj[key];
+                } else {
+                    inject[key] = {
+                        default: obj[key],
+                    };
+                }
+            }
+
+            // return a Vue instance with the same interface as the original class
+            return new vueConstructor({
+                computed,
+                created,
+                data,
+                inject,
+                methods,
+            });
+        }
+    }
+    Object.defineProperty(C, "name", { value: ctor.name });
+
+    return C as any;
+}
+
+type ClassDecorator = <T>(ctor: IConstructable<T>) => IConstructable<T>;
+export function reactive(options: IDecoratorOptions): ClassDecorator;
+export function reactive<T>(ctor: IConstructable<T>): IConstructable<T>;
+export function reactive<T>(arg: IDecoratorOptions | IConstructable<T>): ClassDecorator | IConstructable<T> {
+    if (typeof arg === "function") {
+        return decorate(arg);
+    }
+    return (ctor) => decorate(ctor, arg);
+}
